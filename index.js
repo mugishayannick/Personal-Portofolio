3c7fbe002a8b80c525f38e472199e1590d673c66
mugishayannick <yannickmugisha23@gmail.com>
2022-06-24 15:43:59 +0200

    Add data to local storage

--- index.js
+++ index.js
@@ -21,45 +21,40 @@ if (typeof window !== 'undefined') {
     });
   });
 }
-
-
 const contactForm = document.querySelector('.contact-form');
 const name = document.getElementById('name');
 const text = document.getElementById('text');
 const email = document.getElementById('email');
 const textarea = document.getElementById('textarea');
 contactForm.addEventListener('submit', (e) => {
-alert('Message sent successfully');
-e.preventDefault();
-const name = document.querySelector('#name').value;
-const text = document.querySelector('#text').value;
-const email = document.querySelector('#email').value;
-const textarea = document.querySelector('#textarea').value;
-  if (name.length == 0) {
-    document.querySelector('.error-name').innerHTML = 'Please enter a name';
+  alert('Message sent successfully');
+  e.preventDefault();
+  const name = document.querySelector('#name').value;
+  const text = document.querySelector('#text').value;
+  const email = document.querySelector('#email').value;
+  const textarea = document.querySelector('#textarea').value;
+  if (name.length === 0) {
+    document.querySelector('.error-name').innerHTML='Please enter a name';
     document.querySelector('.error-name').style.color = 'red';
   } else if (text.length === 0) {
-    document.querySelector('.error-text').innerHTML =
-      'Please enter your Last name';
+    document.querySelector('.error-text').innerHTML='Please enter your Last name';
     document.querySelector('.error-text').style.color = 'red';
   } else if (email.length === 0) {
-    document.querySelector('.error-email').innerHTML =
-      'Please enter a valid email';
+    document.querySelector('.error-email').innerHTML='Please enter a valid email';
     document.querySelector('.error-email').style.color = 'red';
   } else if (textarea.length === 0) {
-    document.getElementById('#error_textarea').innerHTML =
-      'Please enter a query';
+    document.getElementById('#error_textarea').innerHTML='Please enter a query';
     document.querySelector('#error_textarea').style.color = 'red';
   } else {
     // contactForm.submit();
     // add data to localstorage
-  var messageObject = {
-    visitorname: name,
-    visitorlastname: text,
-    visitoremail: email,
-    message: textarea,
-  };
-  if (localStorage.getItem('messages')) {
+    const messageObject = {
+      visitorname: name,
+      visitorlastname: text,
+      visitoremail: email,
+      message: textarea,
+    };
+    if (localStorage.getItem('messages')) {
       // append
       const mymessages = JSON.parse(localStorage.getItem('messages')) || [];
       mymessages.push(messageObject);
