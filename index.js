20438ad8ec94bd504f5841969eaee978bb9e1300
mugishayannick <yannickmugisha23@gmail.com>
2022-06-24 15:31:59 +0200

    Add data to local storage

--- index.js
+++ index.js
@@ -35,38 +35,38 @@ const name = document.querySelector('#name').value;
 const text = document.querySelector('#text').value;
 const email = document.querySelector('#email').value;
 const textarea = document.querySelector('#textarea').value;
-if (name.length == 0) {
-  document.querySelector('.error-name').innerHTML = 'Please enter a name';
-  document.querySelector('.error-name').style.color = 'red';
-} else if (text.length === 0) {
-  document.querySelector('.error-text').innerHTML =
-    'Please enter your Last name';
-  document.querySelector('.error-text').style.color = 'red';
-} else if (email.length === 0) {
-  document.querySelector('.error-email').innerHTML =
+  if (name.length == 0) {
+    document.querySelector('.error-name').innerHTML = 'Please enter a name';
+    document.querySelector('.error-name').style.color = 'red';
+  } else if (text.length === 0) {
+    document.querySelector('.error-text').innerHTML =
+      'Please enter your Last name';
+    document.querySelector('.error-text').style.color = 'red';
+  } else if (email.length === 0) {
+    document.querySelector('.error-email').innerHTML =
       'Please enter a valid email';
-  document.querySelector('.error-email').style.color = 'red';
-} else if (textarea.length === 0) {
-  document.getElementById('#error_textarea').innerHTML =
+    document.querySelector('.error-email').style.color = 'red';
+  } else if (textarea.length === 0) {
+    document.getElementById('#error_textarea').innerHTML =
       'Please enter a query';
-  document.querySelector('#error_textarea').style.color = 'red';
-} else {
+    document.querySelector('#error_textarea').style.color = 'red';
+  } else {
     // contactForm.submit();
     // add data to localstorage
-var messageObject = {
-  visitorname: name,
-  visitorlastname: text,
-  visitoremail: email,
-  message: textarea,
-};
-if (localStorage.getItem('messages')) {
+  var messageObject = {
+    visitorname: name,
+    visitorlastname: text,
+    visitoremail: email,
+    message: textarea,
+  };
+  if (localStorage.getItem('messages')) {
   // append
-  var mymessages = JSON.parse(localStorage.getItem('messages')) || [];
-  mymessages.push(messageObject);
-  localStorage.setItem('messages', JSON.stringify(mymessages));
-} else {
-  // set localstorage key and value
-  localStorage.setItem('messages', JSON.stringify(messageObject));
-}
-}
-});
+    var mymessages = JSON.parse(localStorage.getItem('messages')) || [];
+    mymessages.push(messageObject);
+    localStorage.setItem('messages', JSON.stringify(mymessages));
+  } else   {
+    // set localstorage key and value
+    localStorage.setItem('messages', JSON.stringify(messageObject));
+  }
+  }
+  });
